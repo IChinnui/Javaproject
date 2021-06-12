33f3e947c3a18eb1db0ad22566dbc7bf07c103f5
IChinnui <81482965+IChinnui@users.noreply.github.com>
2021-06-12 23:54:56 +0530

    Add files via upload
    Java program to run in jenkins...

---
+++ JavaJGI.java
@@ -0,0 +1,10 @@
+public class JavaJGI
+{
+ public static void main(String[] args)
+  {
+    for(int i=1;i<=10;i++)
+    {
+       System.out.println("Hello Raju welcome to jave..."+i);
+    }
+  }
+}
